a5a117e25dbf48d939b134e51c9adb744b8fcaf7
iamutkarshyadav <72760223+iamutkarshyadav@users.noreply.github.com>
2023-02-12 01:08:09 +0530

    main

--- src/constants/index.js
+++ src/constants/index.js
@@ -48,7 +48,7 @@ export const feedback = [
     id: "feedback-1",
     content:
       "Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver.",
-    name: "Herman Jensen",
+    name: "Ayushi Singh",
     title: "Founder & Leader",
     img: people01,
   },
@@ -56,7 +56,7 @@ export const feedback = [
     id: "feedback-2",
     content:
       "Money makes your life easier. If you're lucky to have it, you're lucky.",
-    name: "Steve Mark",
+    name: "Utkarsh Yadav",
     title: "Founder & Leader",
     img: people02,
   },
@@ -64,7 +64,7 @@ export const feedback = [
     id: "feedback-3",
     content:
       "It is usually people in the money business, finance, and international trade that are really rich.",
-    name: "Kenn Gallagher",
+    name: "Shadow Storme",
     title: "Founder & Leader",
     img: people03,
   },
